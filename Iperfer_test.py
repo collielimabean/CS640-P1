0aba81b6beeb6fb6df1cf0ea7c939078dd943ac6
William Jen <wjen@wisc.edu>
2017-02-05 17:16:56 -0800

    Completed rest of data collection.

--- Iperfer_test.py
+++ Iperfer_test.py
@@ -57,29 +57,85 @@ if __name__ == '__main__':
     net.start()
     h1 = net.getNodeByName("h1")
     h4 = net.getNodeByName("h4")
+    h5 = net.getNodeByName("h5")
+    h6 = net.getNodeByName("h6")
     h7 = net.getNodeByName("h7")
+    h8 = net.getNodeByName("h8")
+    h9 = net.getNodeByName("h9")
     h10 = net.getNodeByName("h10")
 
     # note that for this to work the
     # the Iperfer should be in the working directory
+    """
     print "running Iperfer Servers on h1 and h7"
-    h1.cmd("java Iperfer -s -p 2222 &")
-    h7.cmd("java Iperfer -s -p 2222 &")
-
+    h1.sendCmd("java Iperfer -s -p 2222")
+    h7.sendCmd("java Iperfer -s -p 2222")
+    h8.sendCmd("java Iperfer -s -p 2222")
+    """
+    """
+    h1.sendCmd("ping -c 20 10.0.0.4")
+    h7.sendCmd("ping -c 20 10.0.0.10")
+    h4.sendCmd("ping -c 20 10.0.0.1")
+    h10.sendCmd("ping -c 20 10.0.0.7")
+    h8.sendCmd("ping -c 20 10.0.0.9")
+    h9.sendCmd("ping -c 20 10.0.0.8")
+    """
+    """    
     print "running Iperfer clients on h4 and h10"
     h4.sendCmd("java Iperfer -c -h 10.0.0.1 -p 2222 -t 60")
     h10.sendCmd("java Iperfer -c -h 10.0.0.7 -p 2222 -t 60")
+    h9.sendCmd("java Iperfer -c -h 10.0.0.8 -p 2222 -t 60")
     print "waiting for Iperfer output from hosts"
+    """
 
+    # Q4 servers
+    h4.sendCmd("java Iperfer -s -p 2222")
+    h6.sendCmd("java Iperfer -s -p 2222")
+
+    # Q4 clients
+    h1.sendCmd("java Iperfer -c -h 10.0.0.4 -p 2222 -t 60")
+    h5.sendCmd("java Iperfer -c -h 10.0.0.6 -p 2222 -t 60")
+    #h1.sendCmd("ping -c 20 10.0.0.4")
+    #h5.sendCmd("ping -c 20 10.0.0.6")
+
+
+    res1 = h1.waitOutput()
     res4 = h4.waitOutput()
+    res5 = h5.waitOutput()
+    res6 = h6.waitOutput()
+    
+    """
+    res7 = h7.waitOutput()
+    res8 = h8.waitOutput()
+    res9 = h9.waitOutput()
     res10 = h10.waitOutput()
+    """
 
+    print "h1 result"
+    print res1
 
     print "h4 result"
     print res4
 
+    print "h5 result"
+    print res5
+
+    print "h6 result"
+    print res6
+
+    """
+    print "h7 result"
+    print res7
+ 
+    print "h8 result"
+    print res8
+
+    print "h9 result"
+    print res9
+    
     print "h10 result"
     print res10
+    """
 
     #CLI( net )
     net.stop()
